f8752b132eb1583910dc749131eaf67db909fd64
Jerry <jerryx.xu@mail.utoronto.ca>
2019-10-28 08:15:16 -0400

    XXXXX

---
+++ Assets/Scripts/tutorial_UI/tut_awsd.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class tut_awsd : MonoBehaviour{
+    public GameObject player;
+
+
+    void Start()
+    {
+    }
+    private void Update(){
+        
+    }
+}
