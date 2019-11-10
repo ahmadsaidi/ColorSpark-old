640cd83e5fab2931d45b924518b89c8b33469212
Xuhong Zhong <xuhong.zhong@mail.utoronto.ca>
2019-11-10 13:28:34 -0500

    powerengine tut done

--- Assets/Scripts/tutorial_UI/powerengine_tut.cs
+++ Assets/Scripts/tutorial_UI/powerengine_tut.cs
@@ -9,32 +9,26 @@ public class powerengine_tut : MonoBehaviour
     public Text instruction;
     public int stage;
     public GameObject redEngine;
-    public GameObject yellowEngine;
     public GameObject greenEngine;
     public GameObject blueEngine;
     private GameObject robot;
-    public string[] stageInstructions = {"Put the red power on the power engine to destroy the wall.",
-                                         "Put the yellow power on the power engine to use pre-defined portals.",
-                                         "Press RB in front of pre-defiend portal to use it" ,
-                                         "Put the green power on the power engine to build a brige.",
-                                         "Step on the box right beside the power engine and put the blue power on the power engine to make the box float."};
+    public string[] stageInstructions = {"Activate the portal",
+                                         "Build the bridge",
+                                         "Turn on the elevator" };
 
     private engineController cs0;
     private engineController cs1;
     private engineController cs2;
-    private engineController cs3;
 
     void Start()
     {
-        instruction = GetComponent<Text>();
-        redEngine = GameObject.Find("engine0");
-        yellowEngine = GameObject.Find("engine1");
-        greenEngine = GameObject.Find("engine2");
-        blueEngine = GameObject.Find("engine3");
-        cs0  = redEngine.GetComponent<engineController>();
-        cs1  = yellowEngine.GetComponent<engineController>();
-        cs2  = greenEngine.GetComponent<engineController>();
-        cs3  = blueEngine.GetComponent<engineController>();
+        instruction.text = stageInstructions[0];
+        blueEngine = GameObject.Find("engine0");
+        greenEngine = GameObject.Find("engine1");
+        redEngine = GameObject.Find("engine2");
+        cs0  = blueEngine.GetComponent<engineController>();
+        cs1  = greenEngine.GetComponent<engineController>();
+        cs2  = redEngine.GetComponent<engineController>();
         robot = GameObject.Find("Robot+LED");
         stage = 0;
     }
@@ -45,28 +39,12 @@ public class powerengine_tut : MonoBehaviour
 
         if (cs0.color != Color.white && stage != 1) {
             stage = 1;
-            //GetComponent<Text>().color = Color.yellow;
-
-        };
-
-
+        }
 
         if (cs1.color != Color.white && stage != 2) {
             stage = 2;
+            
         }
-
-        if (robot.transform.position.y > 30 && stage < 3)
-        {
-            stage = 3;
-           // GetComponent<Text>().color = Color.green;
-        }
-
-        if (cs2.color != Color.white && stage != 4) {
-            stage = 4;
-           // instruction.color = Color.blue;
-        }
-        if (instruction != null && instruction.text != null){
-            instruction.text = stageInstructions[stage];
-        }
+        if (instruction != null)instruction.text = stageInstructions[stage];
     }
 }
